5a9c5d60f091bef5cf0cfee626702ce0e8c05b3b
Kleynzit <MKleynzit@Verint.com>
2017-04-04 16:22:19 +0300

    colored some particles

--- particles/comet.js
+++ particles/comet.js
@@ -1,22 +1,22 @@
 var Comet = {
     props: {
-        speed: 3,
+        speed: 1.7,
         x: 0,
         y: 0,
         origX: 0,
         origY: 0,
-        radius: 10,
-        rotation: 100,
+        radius: 5,
+        rotation: 200,
         startTime: null,
         color: '#FFFFFF',
-        particleAge: 100,
-        particleSize: 5
+        particleAge: 200,
+        particleSize: 3
     },
     particles: [],
     draw: function (canvas, context) {
         if (this.props.startTime == null) {
             this.props.startTime = new Date().getTime();
-        }        
+        }
 
         this.reposition(canvas);
 
@@ -24,21 +24,24 @@ var Comet = {
 
         this.drawParticles(context);
 
-        this.particles.push({
-            x: this.props.x + this.props.radius/2 - this.props.particleSize/2,
-            y: this.props.y + this.props.radius/2 - this.props.particleSize/2,
-            age: this.props.particleAge,
-        });
+        for (var i = 0; i < 10; i++) {
+            this.particles.push({
+                x: this.props.x + this.props.radius / 2 - this.props.particleSize / 2,
+                y: this.props.y + this.props.radius / 2 - this.props.particleSize / 2,
+                age: this.props.particleAge,
+            });
+
+        }
     },
     reposition: function (canvas) {
-        var time = (new Date().getTime() - this.props.startTime)/1000 * this.props.speed;
+        var time = (new Date().getTime() - this.props.startTime) / 1000 * this.props.speed;
         this.props.x = this.props.origX + this.props.rotation * Math.sin(time);
-        this.props.y = this.props.origy + this.props.rotation * Math.cos(time);
+        this.props.y = this.props.origY + this.props.rotation * Math.cos(time);
         if (this.props.x >= canvas.width) {
             this.props.x = 0;
         }
         //this.props.y = this.props.y + Math.sin(this.props.startTime)*10;
-        
+
     },
     drawSquare: function (context) {
         context.beginPath();
@@ -46,7 +49,7 @@ var Comet = {
         context.fillStyle = this.props.color;
         context.fill();
     },
-    drawParticles: function (context) {              
+    drawParticles: function (context) {
         var i = this.particles.length - 1;
         while (i >= 0) {
             if (this.particles[i].age > 0) {
@@ -56,14 +59,16 @@ var Comet = {
                 this.particles.splice(i, 1);
             }
             i -= 1;
-        }        
+        }
     },
-    drawSingleParticle: function (context, particle) {    
-        context.beginPath();      
-        context.rect(particle.x, particle.y, this.props.particleSize, this.props.particleSize);                      
-        context.fillStyle = 'rgba(255,255,255,'+particle.age/this.props.particleAge+')';
+    drawSingleParticle: function (context, particle) {
+        context.beginPath();
+        context.rect(particle.x, particle.y, this.props.particleSize, this.props.particleSize);
+        context.fillStyle = 'rgba(255,' + Math.floor((particle.age / this.props.particleAge) * 255) + ',0,'+particle.age / this.props.particleAge+')';
         context.fill();
-        particle.age -=1;
+        particle.age -= 1;
+        particle.y += (Math.random() - 0.5) * 2;
+        particle.x += (Math.random() - 0.5) * 2;
     },
     hex2rgb: function (hex) {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
